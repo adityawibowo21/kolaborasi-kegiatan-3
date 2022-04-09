7d536e8929996f2f398e98a4ff5aa3e96243337a
Aditya Prariyadi Wibowo <101958435+adityawibowo21@users.noreply.github.com>
2022-04-09 19:14:51 +0700

    Update output.h

--- kolaborasi/library/output.h
+++ kolaborasi/library/output.h
@@ -2,32 +2,33 @@ using namespace std;
 
 class Output {
 	public :
-		void cetak(){
-          cout<<"\n\n===============================================\n";
-	    cout<<"|\t\t\t\tSlip Pembayaran"; cout<<"                |\n";       cout<<"===============================================\n";
-      cout<<"|Menu yang dipesan \n";
-      cout<<"|* Ayam Geprek  : "<<data_file[5]<<endl;
-      cout<<"|* Ayam Goreng  : "<<data_file[6]<<endl;;
-      cout<<"|* Udang Goreng : "<<data_file[7]<<endl;;
-      cout<<"|* Cumi Goreng  : "<<data_file[8]<<endl;;
-      cout<<"|* Ayam Bakar   : "<<data_file[9]<<endl;;
-      cout<<"===============================================\n";
-	    cout<<"|Harga Jual     : Rp "<<data_file[0]<<endl;
-	    cout<<"|Diskon         : Rp "<<data_file[1] <<endl;
-      cout<<"|Diskon ongkir  : Rp "<<data_file[2] <<endl;
-      cout<<"|Biaya ongkir   : Rp "<<data_file[3] <<endl;
-	    cout<<"|Total Bayar    : Rp "<<data_file[4] <<endl;
-	    cout<<"===============================================\n";
-		}
+	void cetak(){
+        cout<<"\n\n===============================================\n";
+	cout<<"|\t\t\t\tSlip Pembayaran"; cout<<"                |\n";       
+	cout<<"===============================================\n";
+      	cout<<"|Menu yang dipesan \n";
+      	cout<<"|* Ayam Geprek  : "<<data_file[5]<<endl;
+      	cout<<"|* Ayam Goreng  : "<<data_file[6]<<endl;;
+      	cout<<"|* Udang Goreng : "<<data_file[7]<<endl;;
+      	cout<<"|* Cumi Goreng  : "<<data_file[8]<<endl;;
+      	cout<<"|* Ayam Bakar   : "<<data_file[9]<<endl;;
+      	cout<<"===============================================\n";
+	cout<<"|Harga Jual     : Rp "<<data_file[0]<<endl;
+	cout<<"|Diskon         : Rp "<<data_file[1] <<endl;
+      	cout<<"|Diskon ongkir  : Rp "<<data_file[2] <<endl;
+      	cout<<"|Biaya ongkir   : Rp "<<data_file[3] <<endl;
+	cout<<"|Total Bayar    : Rp "<<data_file[4] <<endl;
+	cout<<"===============================================\n";
+}
 
-		void getData(){
-			ambil_data.open("api_data.txt");
-			while(!ambil_data.eof()){
-				ambil_data >> data_file[index];
-				index += 1;
-			}
-			ambil_data.close();
+	void getData(){
+	ambil_data.open("api_data.txt");
+	while(!ambil_data.eof()){
+		ambil_data >> data_file[index];
+		index += 1;
 		}
+	ambil_data.close();
+	}
 
 	private :
 		ifstream ambil_data;
